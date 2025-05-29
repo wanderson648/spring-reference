02d6b40c0abf91c8a86b2f121a23cab791d9ff2d
wanderson648 <testepedroca@gmail.com>
2025-05-29 20:32:19 -0300

    feat: configura projeto

---
+++ src/test/java/com/devsuperior/dslarn/DslarnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslarn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslarnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
