a58872f51fc60433e675b1c294f5e00067860745
wanderson648 <testepedroca@gmail.com>
2025-05-31 11:51:08 -0300

    feat: Enrollment seed

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -27,3 +27,7 @@ INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) 
 INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capírulo 1', 'Neste capitulo vamos começar', 1, 'https://maiscursoslivres.com.br/cursos/ef7261393731d06e60964b5c3cccd55f.jpg', 1, null)
 INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capírulo 2', 'Neste capitulo vamos continuar', 2, 'https://maiscursoslivres.com.br/cursos/ef7261393731d06e60964b5c3cccd55f.jpg', 1, 1)
 INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capírulo 3', 'Neste capitulo vamos finalizar', 3, 'https://maiscursoslivres.com.br/cursos/ef7261393731d06e60964b5c3cccd55f.jpg', 1, 2)
+
+
+INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (1, 1, TIMESTAMP WITH TIME ZONE '2020-11-20T13:00:00Z', null, true, false)
+INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (2, 1, TIMESTAMP WITH TIME ZONE '2020-11-20T13:00:00Z', null, true, false)
