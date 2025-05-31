ec50be6517987061837f5b5c9e253b7a1de5911e
wanderson648 <testepedroca@gmail.com>
2025-05-31 13:36:17 -0300

    feat: Deliver

---
+++ src/main/java/com/devsuperior/dslarn/entities/Deliver.java
@@ -0,0 +1,42 @@
+package com.devsuperior.dslarn.entities;
+
+import com.devsuperior.dslarn.entities.enums.DeliverStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_deliver")
+public class Deliver {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String uri;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant moment;
+
+    private DeliverStatus status;
+    private String feedback;
+    private Integer correctCount;
+
+    @ManyToMany
+    @JoinColumns({
+            @JoinColumn(name = "offer_id"),
+            @JoinColumn(name = "user_id")
+    })
+    private Enrollment enrollment;
+
+    @ManyToOne
+    @JoinColumn(name = "lesson_id")
+    private Lesson lesson;
+}
---
+++ src/main/java/com/devsuperior/dslarn/entities/enums/DeliverStatus.java
@@ -0,0 +1,5 @@
+package com.devsuperior.dslarn.entities.enums;
+
+public enum DeliverStatus {
+    PENDING, ACCEPTED, REJECTED
+}
